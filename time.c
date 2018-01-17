5927084227b018fd1705e3795087e1112c6d5ced
AKHILASANTHOSH <30549500+AKHILASANTHOSH@users.noreply.github.com>
2018-01-17 15:41:00 +0530

    time.c

---
+++ time.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int h,m,tm;
+printf("enter the hours:");
+scanf("%d",&h);
+printf("enter the minutes:");
+scanf("%d",&m);
+tm=(h*60)+m;
+printf("total time in minutes is:",tm);
+}
