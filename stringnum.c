2c420ea607f65a6089bc7d85e1d11bde322ebe88
AKHILASANTHOSH <30549500+AKHILASANTHOSH@users.noreply.github.com>
2018-01-17 16:37:35 +0530

    stringnum.c

---
+++ stringnum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int string[30],i;
+int flag=0;
+printf("enter the string:");
+for(i=o;i<=s[i];i++)
+scanf("%s",s[i]);
+if(s[i]=='0'||s[i]=='1'||s[i]=='2'||s[i]=='3'||s[i]=='4'||s[i]=='5'||s[i]=='6'||s[i]=='7'||s[i]=='8'||s[i]=='9')
+{
+printf("the string is numeric");
+}
+else
+{
+printf("the string is not numeric");
+}
+getch();
+}
