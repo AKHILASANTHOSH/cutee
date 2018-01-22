9485cd5c84502ba7bbf423fa1e08dbd6193bca84
AKHILASANTHOSH <30549500+AKHILASANTHOSH@users.noreply.github.com>
2018-01-22 16:40:47 +0530

    39.c

---
+++ 39.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a[20],i,grt;
+printf("enter the numbersL:");
+for(i=0;i<10;i++)
+{
+scanf("%d",a[i]);
+}
+grt=a[0];
+for(i=0;i<10;i++)
+{
+if(a[i]>grt)
+{
+grt=a[i];
+printf("greatest number is:%d",grt);
+}
+}
+}
