9a1319de5624bff5c28a91d9ba340ee5bb06e4a2
AKHILASANTHOSH <30549500+AKHILASANTHOSH@users.noreply.github.com>
2018-01-22 16:17:14 +0530

    38.c

---
+++ 38.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void swap(int *x,int *y);
+{
+temp=*x;
+*x=*y;
+*y=temp;
+}
+int main()
+{
+void swap(int *x,int *y)
+{
+int x,y,temp;
+printf("enter the value of x:");
+scanf("%d",&x);
+printf("enter the value of y:");
+scanf('%d",&y);
+printf("after swapping:%d%d",x,y);
+}
+}
+
