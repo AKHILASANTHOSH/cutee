252ebe1000c95a92bb1d8dbd73b646276d0a6e2e
AKHILASANTHOSH <30549500+AKHILASANTHOSH@users.noreply.github.com>
2018-01-22 15:59:53 +0530

    cal.java

---
+++ cal.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+public class cal
+{
+public static void main(String args[])
+{
+Calender c=new Calender.getInstance();
+Calender new=Calender.getInstance();
+System.out.println("current date:"+"current date"+"current month"+"current year");
+System.out.println("cuttent minute:"+"current minutes"+"current seconds");
+c=Calender.getInstance();
+n=add(Calender.hour);
+System.out.println("time in format");
+}
+}
