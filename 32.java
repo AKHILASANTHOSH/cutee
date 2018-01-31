8e535fe1dd12c4ab9446be917b46534882b94a72
AKHILASANTHOSH <30549500+AKHILASANTHOSH@users.noreply.github.com>
2018-01-31 15:47:55 +0530

    32.java

---
+++ 32.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class 32
+{
+public static void main(String agrs[])
+{
+int count=0;
+String a;
+Scanner sc=new Scanner(System.in);
+a=sc.nextline();
+String aa=a.splt("\\sc+");
+for(i=0;i<length;i++)
+{
+count++;
+}
+}
+}
