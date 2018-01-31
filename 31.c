32e7e2bc907c8ec962d25aeb7d85f92a0bd9f1e4
AKHILASANTHOSH <30549500+AKHILASANTHOSH@users.noreply.github.com>
2018-01-31 15:40:03 +0530

    31.c

---
+++ 31.c
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.math.*;
+import java.util.Scanner.*;
+public class a
+{
+public static void main(Struing args[])
+{
+String str="i love programming";
+int count=0;
+Scanner sc=new Scanner(System.in);
+st=sc.nextLine();
+for(int i=0;i<st.length();i++)
+{
+count++;
+}
+System.out.println("count");
+}
+}
